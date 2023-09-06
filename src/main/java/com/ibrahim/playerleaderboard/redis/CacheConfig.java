9bc2dfe8cd8ba2e1f174d55105fb69feafe83ecc
Ibrahim Diba <ifraibrahim60@gmail.com>
2023-09-06 13:34:57 +0300

    Used env variables for docker

--- src/main/java/com/ibrahim/playerleaderboard/redis/CacheConfig.java
+++ src/main/java/com/ibrahim/playerleaderboard/redis/CacheConfig.java
@@ -1,26 +1,45 @@
 package com.ibrahim.playerleaderboard.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import java.time.Duration;
-
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
+    @Value("${redis-host}")
+    private String redisHost;
+
+    @Value("${redis-port}")
+    private int redisPort;
+
+    @Value("${default-ttl}")
+    private Long cacheTtlMinutes;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(config);
+    }
+
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        // Use the custom cache TTL from properties
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(5)); // Cache entries for 10 minutes
+                .entryTtl(Duration.ofMinutes(cacheTtlMinutes));
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
                 .build();
     }
 }
+
